fc50150b25756ae913b797a7b86635de636eb794
joshuapereda <joshuadesihuas@gmail.com>
2024-09-11 22:11:03 -0500

    Mi primer commit

---
+++ src/main/java/com/prestamo/Dawi202405Semana01Application.java
@@ -0,0 +1,13 @@
+package com.prestamo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dawi202405Semana01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dawi202405Semana01Application.class, args);
+	}
+
+}
